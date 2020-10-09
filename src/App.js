4760355ab83e8c9ec046864e1c1d5892fa85fb9f
Evisari Riyanto <evisari111012@gmail.com>
2020-10-09 15:51:43 -0700

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import Results from "./Components/Results/Results";
 
 function App() {
-  return <div className="App">Star Wars Search</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Results />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Header() {
+  return <h1>Star Wars Search</h1>;
+}
